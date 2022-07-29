e67c10b69d280f07f690b56e3d84a6278709e58e
Diane Lopez <70670592+dianita956@users.noreply.github.com>
2022-07-29 18:29:10 -0500

    add code file

---
+++ hello.py
@@ -0,0 +1,31 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class GridLayout(GridLayout):
+    #initialize infinite keywords
+    """docstring for ."""
+
+    def __init__(self, **kwargs):
+        #call gridlayoyt constructor
+        super(GridLayout, self).__init__(**kwargs)
+        #mapping columns and rows
+        self.cols = 2
+
+        #add widget
+        self.add_widget(Label(text="Name: "))
+
+        #add input box
+        self.name = TextInput(multiline=False)
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text="hello world", font_size=72)
+#adding comment
+
+if __name__ == '__main__':
+    MyApp().run()
